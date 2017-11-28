8b3b83400a3a3e4722b35b42c7bffbc3c3a5f79a
deejaycode <dennis.ozj@gmail.com>
2017-11-28 16:28:43 +0100

    dbconn

--- dbconn.php
+++ dbconn.php
@@ -13,6 +13,23 @@
 			this->$_connection = mysqli_connect($_host, $_username, $_password, $_dbname);
 
 		}
+
+
+		public function getResult() {
+
+			$this->result = new Result ($this->$_connection)
+		}
+	}
+
+	class Result {
+
+		private $_result;
+
+		public function __construct($db)
+		{
+			$db_query = mysqli_query($db, "SELECT * FROM books") or die (mysqli_error($db));
+			$this->_result = mysqli_fetch_array($db_query));
+		} 
 	}
 
 
