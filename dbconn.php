f89cdc9ce065f9ebe02b7e3dc47ef50a9cb0972f
deejaycode <dennis.ozj@gmail.com>
2017-11-28 18:35:56 +0100

    done with OOP connection object

--- dbconn.php
+++ dbconn.php
@@ -1,40 +1,32 @@
 <?php
 
-	class DBconn {
+	class MYSQLIConnection {
 
 		private $_connection;
-		private $_host;
-		private $_username;
-		private $_password;
-		private $_dbname;
-
-		public function __construct($_host, $_password, $_username, $_dbname) {
-
-			this->$_connection = mysqli_connect($_host, $_username, $_password, $_dbname);
-
-		}
-
-
-		public function getResult() {
-
-			$this->result = new Result ($this->$_connection)
-		}
-	}
-
-	class Result {
-
 		private $_result;
 
-		public function __construct($db)
-		{
-			$db_query = mysqli_query($db, "SELECT * FROM books") or die (mysqli_error($db));
-			$this->_result = mysqli_fetch_array($db_query));
-		} 
+		public function __construct() {
+
+			require_once('dbconfig.php');
+
+			extract($config);
+
+			/*echo $user;
+			exit();
+*/
+			$this->_connection = mysqli_connect($host, $user, $password, $dbname);
+
+		}
+
+
+		public function _query($sql) {
+
+			$this->_result = new MYSQLIResult($this->_connection, $sql);
+
+			return $this->_result;
+		}
+
 	}
-
-
-
-
 
 
 ?>
