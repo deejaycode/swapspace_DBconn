cd8a156d938dc91668c3b0d07446b15f17a49f8d
deejaycode <dennis.ozj@gmail.com>
2017-11-28 15:59:13 +0100

    constructor on db connection

--- dbconn.php
+++ dbconn.php
@@ -4,10 +4,18 @@
 
 		private $_connection;
 		private $_host;
-		private $_password;
 		private $_username;
+		private $_password;
 		private $_dbname;
+
+		public function __construct($_host, $_password, $_username, $_dbname) {
+
+			this->$_connection = mysqli_connect($_host, $_username, $_password, $_dbname);
+
+		}
 	}
+
+
 
 
 
